223d606d1893611f471f511a6856cd2c396806f3
winnapat <GGgun2013@gmail.com>
2021-08-05 14:51:02 +0700

    near Finish

--- script.js
+++ script.js
@@ -7,31 +7,68 @@ const color = document.querySelector('#color')
 // define more constants and variables here
 const btn_reset = document.querySelector('#reset')
 const btn_search = document.querySelector('#search')
-const text = text.document.querySelector('#text')
+const text = document.querySelector('#text')
 
-// author.innerHTML = '630610761 WINNAPAT KAEWMORA'
+author.innerHTML = '630610761 WINNAPAT KAEWMORA'
 
 btn_toggle.onclick = () => {
   if(btn_toggle.innerHTML === 'Show Calculation'){
     let name = author.innerHTML
     let number = /\d+/
+    
     calculation.innerHTML = name.match(number)+length.value
-    btn_toggle.innerHTML = "Display author"
-    // author.style.display = "none"
-    // calculation.style.display = "block"
+    btn_toggle.innerHTML = "Show author"
+    author.style.display = "none"
+    calculation.style.display = "block"
   }
   else{
     btn_toggle.innerHTML = 'Show Calculation'
-    // author.style.display = "block"
-    // calculation.style.display = "none"
+    author.style.display = "block"
+    calculation.style.display = "none"
   }
 }
 
 // more codes for Search and Reset buttons here
 btn_search.onclick = () => {
-  ()
+  text.innerHTML = highlight()
 }
 
 btn_reset.onclick = () => {
-  ()
+  text.innerHTML = reset()
 }
+
+function highlight(){
+  let textoutput = ""
+  let replacetext = ""
+  const data = text.textContent.split(" ")
+  console.log(data)
+  for(let i =0;i < data.length;i++){
+    let word = data[i]
+    if(word.length > length.value){
+      replacetext = "<span style='color:"+color.value+"'>"+word+"</span>"
+    }
+    else{
+      replacetext = word
+    }
+    textoutput += " "+replacetext+" "
+  }
+  return textoutput
+}
+
+function reset(){
+  let textoutput = ""
+  let replacetext = ""
+  const data = text.textContent.split(" ")
+  console.log(data)
+  for(let i =0;i < data.length;i++){
+    let word = data[i]
+    if(word.length > length.value){
+      replacetext = "<span style='color:black'>"+word+"</span>"
+    }
+    else{
+      replacetext = word
+    }
+    textoutput += " "+replacetext+" "
+  }
+  return textoutput
+  }
